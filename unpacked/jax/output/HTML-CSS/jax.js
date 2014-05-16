1bf64a8736a18b961ae9c524681b853cbbcd8f75
Stuart McWilliams <stuart@texthelp.com>
2014-05-16 10:45:50 +0100

    Change to allow highlighting
    Change to allow highlighting.  Expanded HTMLhandleColor, HTMLremoveColor
    and added Highlight object to specify what style to highlight with.

--- unpacked/jax/output/HTML-CSS/jax.js
+++ unpacked/jax/output/HTML-CSS/jax.js
@@ -1823,7 +1823,9 @@
         return span;
       },
 
-      HTMLhandleColor: function (span) {
+      // span is required, marker is optional string.  If given it is added to id of element created to make it unique.
+      // Without this, calling this multiple times with the same span will result in elements with non unique ID
+      HTMLhandleColor: function (span, marker) {
 	var values = this.getValues("mathcolor","color");
 	if (this.mathbackground) {values.mathbackground = this.mathbackground}
 	if (this.background) {values.background = this.background}
@@ -1834,9 +1836,14 @@
         var borders = (this.styles||{}).border, padding = (this.styles||{}).padding;
 	if (values.color && !this.mathcolor) {values.mathcolor = values.color}
 	if (values.background && !this.mathbackground) {values.mathbackground = values.background}
-	if (values.mathcolor) {span.style.color = values.mathcolor}
+	if (values.mathcolor || values.mathcolor === "") {span.style.color = values.mathcolor}
+        var mathOutline = this.mathoutline;
+        var mathUnderline = this.mathunderline;
+        if(typeof(mathUnderline) === "string") {
+          span.style.textDecoration = mathUnderline;
+        }
 	if ((values.mathbackground && values.mathbackground !== MML.COLOR.TRANSPARENT) || 
-             borders || padding) {
+             borders || padding || mathOutline) {
 	  var bbox = span.bbox, dd = (bbox.exact ? 0 : 1/HTMLCSS.em), lW = 0, rW = 0,
               lpad = span.style.paddingLeft, rpad = span.style.paddingRight;
 	  if (this.isToken) {lW = bbox.lw; rW = bbox.rw - bbox.w}
@@ -1860,6 +1867,29 @@
             bbox.lw -= padding.left; bbox.rw += padding.right;
           }
           if (rp) {span.style.paddingRight = HTMLCSS.Em(rp)}
+
+          var strSpanID;
+          if(typeof(marker) === "string") {
+            strSpanID =  "MathJax-Color-" + this.spanID + "-" + marker + HTMLCSS.idPostfix;
+            if(document.getElementById(strSpanID) != null) {   // when marker given ensure that the ID is unique
+              return null;
+            }
+          }
+          else {
+            strSpanID = "MathJax-Color-" + this.spanID + HTMLCSS.idPostfix;
+          }
+
+          var styleObj = {
+            display:"inline-block", backgroundColor:values.mathbackground,
+            width: HTMLCSS.Em(W), height:HTMLCSS.Em(H), verticalAlign: HTMLCSS.Em(D),
+            marginLeft: HTMLCSS.Em(lW), marginRight: HTMLCSS.Em(rW)
+          };
+          if(typeof(mathOutline) === "string")
+          {
+            styleObj["outline"] = mathOutline;
+          }
+
+
 	  var frame = HTMLCSS.Element("span",{
             id:"MathJax-Color-"+this.spanID+HTMLCSS.idPostfix, isMathJax: true,
 	    style:{display:"inline-block", backgroundColor:values.mathbackground,
@@ -1890,8 +1920,10 @@
 	}
 	return null;
       },
-      HTMLremoveColor: function () {
-	var color = document.getElementById("MathJax-Color-"+this.spanID+HTMLCSS.idPostfix);
+      HTMLremoveColor: function (marker) {
+        var strSpanID =  "MathJax-Color-" + this.spanID + "-" + ((typeof(marker) === "string")?marker:"") +
+            HTMLCSS.idPostfix;
+        var color = document.getElementById(strSpanID);
 	if (color) {color.parentNode.removeChild(color)}
       },
 
@@ -3028,5 +3060,384 @@
     if (HUB.config.menuSettings.zoom !== "None")
       {AJAX.Require("[MathJax]/extensions/MathZoom.js")}
   });
-    
+
+  HUB.Highlight = {
+    elementHelper: (function () {
+      function LoopState() {
+        this.count = 2; // When loop starts already at the second element
+        this.list = [];
+      }
+
+      // Recursive loop through the dom.
+      // Adds elements in given range to the list
+      // Returns true when gone as far as it needs to go.
+      function looper(dataElem, start, end, state) {
+        if (!dataElem.inferred) {
+          // if is multi character token is handled separately
+          var bHandledToken = false;
+
+          // If it is a token, could have multiple characters in a single item.  These are all references by separate
+          // numbers for the start/end values even though they are a single math element. Just add the element once
+          // even if matched several times.
+          if (dataElem.isToken) {
+            if (dataElem.data[0] && dataElem.data[0].type === "chars" && typeof(dataElem.data[0].data[0]) === "string") {
+              var token = dataElem.data[0].data[0];
+              if (token.length > 1) {
+                var isMatch = false;
+                for (var i = 0, nLen = token.length; i < nLen; i++) {
+                  ++state.count;
+                  if (state.count >= start && state.count <= end) {
+                    isMatch = true;
+                  }
+                }
+                if (isMatch) {
+                  state.list.push(dataElem);
+                }
+                bHandledToken = true;
+              }
+            }
+          }
+
+          if (!bHandledToken) {
+            ++state.count;
+            if (state.count >= start && state.count <= end) {
+              state.list.push(dataElem);
+            }
+          }
+        }
+
+        if (state.count >= end) {
+          return true;
+        }
+
+        if (dataElem.type === "mspace") {   // don't go to children of this if any (based on toMathML skipping this
+          return false;
+        }
+
+        for (var i = 0, nLen = dataElem.data.length; i < nLen; i++) {
+          if (dataElem.data[i].type !== "chars") {
+            if (looper(dataElem.data[i], start, end, state)) {
+              return true;
+            }
+          }
+        }
+
+        return false;
+      }
+
+      return {
+        // Get list of math jax elements.
+        // Pass in the root from the jax object for the equation.
+        // Then the start and end are the count of mathml elements, starting at 1 for the math element.
+        getList: function (root, start, end) {
+          var state = new LoopState();
+
+          if (start === 1) {
+            state.list.push(root);
+          }
+
+          looper(root.data[0], start, end, state);
+
+          return state.list;
+        }
+      };
+    })(),
+
+    // Pass in the jax object of the equation that is to be highlighted
+    getHighlighter: (function () {
+      // add in hash and class and only return function that uses them
+      var highlighterHash = {};
+
+      var Highlighter = function (jax) {
+        var HighlightData = function () {
+          this.style = null;
+          this.fg = null;
+          this.bg = null;
+          this.lastStart = 0;
+          this.lastEnd = 0;
+        };
+        var highlightDataStore = {}; // stores highlight data for given type.
+        var highlightJaxRoot = jax.root;
+        var highlightClearLast = true;
+
+        // Set whether or not the previous highlight should be cleared when adding new one.
+        // If set to false and highlights added the old highlights are not stored and adding a new highlight
+        // does not clear the old one.  Calling clearHighlights() will not remove them.  Setting this to true
+        // will only affect highlights added after it is set true.
+        // The caller needs to keep its own record of highlights added while this is false.
+        this.setClearLastHighlight = function (b) {
+          highlightClearLast = b;
+        };
+
+        // type "context" or "word" or custom value matched to highlightNodes call.
+        // style  "outline", "underline", "fg" or "bg"
+        // r g b value 0-255
+        // returns true if style updated, false otherwise
+        this.setHighlightStyle = function (type, style, r, g, b) {
+          if (typeof(type) !== "string") {
+            return false;
+          }
+
+          // need to check if there is currently highlighting for this type, if so need to remove this
+          // prior to changing or removing the style for this type.
+          if (highlightDataStore[type] && highlightDataStore.hasOwnProperty(type)) {
+            var tmpData = highlightDataStore[type];
+            if (tmpData && tmpData.lastStart > 0 && tmpData.lastStart > 0) {
+              clearHighlightNodesImpl(type, tmpData.style, tmpData.lastStart, tmpData.lastEnd)
+            }
+          }
+
+          if (style === null) {
+            delete highlightDataStore[type];
+            return true;
+          }
+
+          if (typeof(style) === "string") {
+            style = style.toLowerCase();
+          }
+          else {
+            return false;
+          }
+
+          if (style === "") {
+            delete highlightDataStore[type];
+            return true;
+          }
+
+          var nR, nG, nB;
+          try {
+            nR = parseColor(r);
+            nG = parseColor(g);
+            nB = parseColor(b);
+          }
+          catch (err) {   // invalid values passed in
+            return false;
+          }
+
+          var data;
+
+          if (highlightDataStore[type] && highlightDataStore.hasOwnProperty(type)) {
+            data = highlightDataStore[type];
+            // If is already highlight and adding highlight might want existing fg or bg kept.
+            // if existing style is not highlight, always want to clear values.
+            if (data.style !== "highlight") {
+              data.fg = null;
+              data.bg = null;
+            }
+          }
+          else {
+            data = new HighlightData();
+          }
+
+          if (style === "fg") {
+            data.style = "highlight";
+            data.fg = "rgb(" + nR + "," + nG + "," + nB + ")";
+          }
+          else if (style === "bg") {
+            data.style = "highlight";
+            data.bg = "rgb(" + nR + "," + nG + "," + nB + ")";
+          }
+          else if (style === "outline") {
+            data.style = style;
+            data.fg = "rgb(" + nR + "," + nG + "," + nB + ")";
+            data.bg = null;
+          }
+          else if (style === "underline") {
+            data.style = style;
+            data.fg = "rgb(" + nR + "," + nG + "," + nB + ")";
+            data.bg = null;
+          }
+
+          highlightDataStore[type] = data;
+          return true;
+        };
+
+        // Add highlighting to elements from given range
+        // type is used to select the style set for that type with setHighlightStyle (i.e. "context" or "word")
+        // start and end give the inclusive range to highlight.
+        this.highlightNodes = function (type, start, end) {
+          var data;
+          if (highlightDataStore[type] && highlightDataStore.hasOwnProperty(type)) {
+            data = highlightDataStore[type];
+          }
+          else {
+            return;
+          }
+          if (data.style === null) {
+            return;
+          }
+
+          if (data.lastStart > 0 && data.lastEnd > 0 && highlightClearLast) {
+            clearHighlightNodesImpl(type, data.style, data.lastStart, data.lastEnd);
+          }
+
+          data.lastStart = start;
+          data.lastEnd = end;
+
+          // Pass root, then count for position of element for the start and end point to fetch, inclusive.
+          var list = MathJax.Hub.Highlight.elementHelper.getList(highlightJaxRoot, start, end);
+
+          highlightImpl(list, type, data.style, data.fg, data.bg);
+        };
+
+        // clear all highlights
+        this.clearHighlights = function () {
+          for (var type in highlightDataStore) {
+            if (highlightDataStore.hasOwnProperty(type)) {
+              var data = highlightDataStore[type];
+              if (data.lastStart > 0 && data.lastEnd > 0) {
+                clearHighlightNodesImpl(type, "", data.lastStart, data.lastEnd);
+                data.lastStart = 0;
+                data.lastEnd = 0;
+              }
+            }
+          }
+        };
+
+        this.clearHighlightNodes = function (type, start, end) {
+          clearHighlightNodesImpl(type, "", start, end);
+        };
+
+        function parseColor(val) {
+          var tmpVal;
+          if (typeof(val) === "string") {
+            tmpVal = parseInt(val, 10);
+          }
+          else if (typeof(val) === "number") {
+            tmpVal = val;
+          }
+          else {
+            throw "invalid";
+          }
+          if (tmpVal < 0 || tmpVal > 255) {
+            throw "invalid";
+          }
+
+          return tmpVal;
+        }
+
+        function clearHighlightNodesImpl(type, style, start, end) {
+          var list = MathJax.Hub.Highlight.elementHelper.getList(highlightJaxRoot, start, end);
+
+          for (var i = 0, nLen = list.length; i < nLen; i++) {
+            var item = list[i];
+            if (typeof(item.HTMLremoveColor) == "function") {
+              if (style === "" || style === "outline") {
+                delete item.mathoutline;
+              }
+
+              if (style === "" || style === "underline") {
+                if (item.previousline) {
+                  removeFromPrevious(item.previousline, type);
+                  // set to what is left as last in list
+                  var len = item.previousline.length;
+                  if(len > 0) {
+                    item.mathunderline = item.previousline[len-1].val;
+                  } else {
+                    item.mathunderline = "";
+                  }
+                }
+                else {
+                  item.mathunderline = "";
+                }
+              }
+
+              if (style === "" || style === "highlight") {
+                if (item.previouscolor) {
+                  removeFromPrevious(item.previouscolor, type);
+                  // set color to what is left as last in list
+                  var len = item.previouscolor.length;
+                  if(len > 0) {
+                    item.mathcolor = item.previouscolor[len-1].val;
+                  } else {
+                    item.mathcolor = "";
+                  }
+                }
+                else {
+                  item.mathcolor = "";
+                }
+                delete item.mathbackground;
+              }
+
+              // fix colour
+              item.HTMLhandleColor(item.HTMLspanElement(), type);
+
+              // remove the background
+              item.HTMLremoveColor(type);
+            }
+          }
+        }
+
+        function highlightImpl(list, type, style, fg, bg) {
+          for (var i = 0, nLen = list.length; i < nLen; i++) {
+            var item = list[i];
+            if (typeof(item.HTMLhandleColor) == "function") {
+              var spanElem = item.HTMLspanElement();
+
+              if (style === "highlight") {
+                if (bg != null) {
+                  item.mathbackground = bg;
+                }
+
+                if (fg != null) {
+                  if(!item.previouscolor) {
+                    item.previouscolor = [];
+                    if (item.mathcolor) {
+                      item.previouscolor.push({"key":"","val":item.mathcolor});
+                    }
+                  } else {
+                    removeFromPrevious(item.previouscolor, type);
+                  }
+
+                  item.previouscolor.push({"key":type,"val":fg});
+                  item.mathcolor = fg;
+                }
+              }
+              else if (style === "outline") {
+                item.mathoutline = fg + "thin solid";
+              }
+              else if (style === "underline") {
+                if(!item.previousline) {
+                  item.previousline = [];
+                  if (item.textDecoration) {
+                    item.previousline.push({"key":"","val":item.textDecoration});
+                  }
+                } else {
+                  removeFromPrevious(item.previousline, type);
+                }
+                item.previousline.push({"key":type,"val":"underline"});
+
+                item.mathunderline = "underline";
+                // Currently cannot set text-decoration color so color ignored for this
+              }
+
+              item.HTMLhandleColor(spanElem, type);
+            }
+          }
+        }
+
+        function removeFromPrevious(arr, type)
+        {
+          for(var i=0; i<arr.length; i++)
+          {
+            if(arr[i].key === type)
+            {
+              arr.splice(i, 1);
+              return;
+            }
+          }
+        }
+      };
+
+      return function (jax) {
+        if (highlighterHash[jax.inputID] && highlighterHash.hasOwnProperty(jax.inputID)) {
+          return highlighterHash[jax.inputID];
+        } else {
+          return (highlighterHash[jax.inputID] = new Highlighter(jax));
+        }
+      }
+    })()
+
+  };
+
 })(MathJax.Ajax, MathJax.Hub, MathJax.OutputJax["HTML-CSS"]);
